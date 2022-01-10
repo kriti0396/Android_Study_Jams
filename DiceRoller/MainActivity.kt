535a0d8d776cbd413b4450da2760f10c10a1bc86
kriti0396 <81351069+kriti0396@users.noreply.github.com>
2022-01-10 12:38:26 +0530

    Update MainActivity.kt

--- DiceRoller/MainActivity.kt
+++ DiceRoller/MainActivity.kt
@@ -15,6 +15,22 @@ class MainActivity : AppCompatActivity() {
         val button: Button = findViewById(R.id.button);
         val textview: TextView = findViewById(R.id.textview)
         val imageview: ImageView= findViewById(R.id.imageView)
+        
+         imageview.setOnClickListener{
+            var c :Int =(1..6).random()
+
+            when(c)
+            {
+                1->imageview.setImageResource(R.drawable.dice_1)
+                2->imageview.setImageResource(R.drawable.dice_2)
+                3->imageview.setImageResource(R.drawable.dice_3)
+                4->imageview.setImageResource(R.drawable.dice_4)
+                5->imageview.setImageResource(R.drawable.dice_5)
+                6->imageview.setImageResource(R.drawable.dice_6)
+                else->imageview.setImageResource(R.drawable.dice_6)
+            }
+            textview.text=c.toString()
+        }
 
         button.setOnClickListener{
             var c :Int =(1..6).random()
